4645caba6d07c6ba6807abac25652b0a901d1b98
Carlos Lopez <633073+carlosjln@users.noreply.github.com>
2026-03-14 02:05:18 -0400

    streamline update compiler

--- src/model/factory/constants.js
+++ src/model/factory/constants.js
@@ -1,22 +1,27 @@
 const update_path_root_segment_pattern = /^[a-zA-Z_][a-zA-Z0-9_]*$/;
 const update_path_nested_segment_pattern = /^(?:[a-zA-Z_][a-zA-Z0-9_]*|[0-9]+)$/;
+
 const DocumentInputMode = Object.freeze({
 	From: 'from',
 	Hydrate: 'hydrate'
 });
+
 const base_field_keys = new Set(['id', 'created_at', 'updated_at']);
 const timestamp_fields = new Set(['created_at', 'updated_at']);
 const unsafe_from_keys = new Set(['__proto__', 'constructor', 'prototype']);
+
 const update_operator_names = Object.freeze({
 	set: '$set',
 	insert: '$insert',
 	set_lax: '$set_lax'
 });
+
 const update_operator_order = Object.freeze([
 	update_operator_names.set,
 	update_operator_names.insert,
 	update_operator_names.set_lax
 ]);
+
 const set_lax_null_treatments = Object.freeze({
 	raise_exception: 'raise_exception',
 	use_json_null: 'use_json_null',
--- src/model/factory/update-compiler.js
+++ src/model/factory/update-compiler.js
@@ -1,11 +1,9 @@
-import where_compiler from '#src/query/where-compiler/index.js';
-
 import sql_runner from '#src/sql/sql-runner.js';
+import where_compiler from '#src/query/where-compiler/index.js';
 import {bind_parameter, create_parameter_state} from '#src/sql/parameter-binder.js';
 
-import {quote_identifier} from '#src/utils/assert.js';
 import {has_own} from '#src/utils/object.js';
-
+import {quote_identifier} from '#src/utils/assert.js';
 import {assert_supported_update_definition, normalize_update_operator_entries} from '#src/model/factory/update-helpers.js';
 
 /**
@@ -18,19 +16,19 @@ import {assert_supported_update_definition, normalize_update_operator_entries} f
  */
 async function compile_update_one(model, query_filter, update_definition) {
 	const schema = model.schema;
+	const id_strategy = model.id_strategy;
 	const model_options = model.$options;
+	const data_column = model_options.data_column;
+	const table_name = model_options.table_name;
 	const update_operator_entries = normalize_update_operator_entries(update_definition);
 
 	assert_supported_update_definition(update_definition, update_operator_entries);
 
-	const id_strategy = model.id_strategy;
-	const data_column = model_options.data_column;
-	const table_identifier = quote_identifier(model_options.table_name);
+	const table_identifier = quote_identifier(table_name);
 	const data_identifier = quote_identifier(data_column);
-
 	const where_result = where_compiler(query_filter, {schema, data_column, id_strategy});
 	const parameter_state = create_parameter_state(where_result.next_index);
-	const apply_result = build_update_expression(update_operator_entries, data_identifier, parameter_state, schema);
+	const update_expression = build_update_expression(update_operator_entries, data_identifier, parameter_state, schema);
 
 	const query_context = {
 		model,
@@ -38,71 +36,66 @@ async function compile_update_one(model, query_filter, update_definition) {
 		data_identifier,
 		where_result,
 		parameter_state,
-		apply_result
+		update_expression
 	};
 
-	const rows = await exec_update_query(query_context);
+	const row = await exec_update_query(query_context);
 
-	if(rows.length === 0) {
-		return null;
+	if(row) {
+		return model.hydrate(row);
 	}
 
-	return model.hydrate(rows[0]);
+	return null;
 }
 
 function build_update_expression(update_operator_entries, data_expression, parameter_state, schema) {
-	const apply_result = {
-		data_expression,
-		timestamp_set: {}
-	};
+	const update_expression = {data_expression, timestamp_set: {}};
 
 	for(const update_operator_entry of update_operator_entries) {
-		const operator_apply_result = update_operator_entry.operator_descriptor.apply({
+		const operator_result = update_operator_entry.operator_descriptor.apply({
 			definition: update_operator_entry.definition,
-			data_expression,
+			data_expression: update_expression.data_expression,
 			parameter_state,
 			schema
 		});
 
-		apply_result.data_expression = operator_apply_result.data_expression;
+		update_expression.data_expression = operator_result.data_expression;
 
-		if(operator_apply_result.timestamp_set) {
-			Object.assign(apply_result.timestamp_set, operator_apply_result.timestamp_set);
+		if(operator_result.timestamp_set) {
+			Object.assign(update_expression.timestamp_set, operator_result.timestamp_set);
 		}
 	}
 
-	return apply_result;
+	return update_expression;
 }
 
 function build_update_assignments(query_context) {
-	const row_update_assignments = [
-		query_context.data_identifier + ' = ' + query_context.apply_result.data_expression
-	];
-	const timestamp_set = query_context.apply_result.timestamp_set;
+	const update_assignments = [query_context.data_identifier + ' = ' + query_context.update_expression.data_expression];
+	const timestamp_set = query_context.update_expression.timestamp_set;
 	const parameter_state = query_context.parameter_state;
 
 	if(has_own(timestamp_set, 'created_at')) {
 		const created_at_placeholder = bind_parameter(parameter_state, timestamp_set.created_at);
-		row_update_assignments.push('created_at = ' + created_at_placeholder + '::timestamptz');
+		update_assignments.push('created_at = ' + created_at_placeholder + '::timestamptz');
 	}
 
 	if(has_own(timestamp_set, 'updated_at')) {
 		const updated_at_placeholder = bind_parameter(parameter_state, timestamp_set.updated_at);
-		row_update_assignments.push('updated_at = ' + updated_at_placeholder + '::timestamptz');
+		update_assignments.push('updated_at = ' + updated_at_placeholder + '::timestamptz');
 	} else {
-		row_update_assignments.push('updated_at = NOW()');
+		update_assignments.push('updated_at = NOW()');
 	}
 
-	return row_update_assignments;
+	return update_assignments;
 }
 
 async function exec_update_query(query_context) {
-	const row_update_assignments = build_update_assignments(query_context);
+	const update_assignments = build_update_assignments(query_context);
 	const sql_text =
 		'WITH target_row AS (' + 'SELECT id FROM ' + query_context.table_identifier + ' WHERE ' +
 		query_context.where_result.sql + ' LIMIT 1' + ') ' +
 		'UPDATE ' + query_context.table_identifier + ' AS target_table ' +
-		'SET ' + row_update_assignments.join(', ') + ' ' +
+		'SET ' + update_assignments.join(', ') + ' ' +
 		'FROM target_row ' +
 		'WHERE target_table.id = target_row.id ' +
 		'RETURNING target_table.id::text AS id, ' +
@@ -111,8 +104,13 @@ async function exec_update_query(query_context) {
 		'target_table.updated_at AS updated_at';
 	const sql_params = query_context.where_result.params.concat(query_context.parameter_state.params);
 	const query_result = await sql_runner(sql_text, sql_params, query_context.model.connection);
+	const rows = query_result.rows;
 
-	return query_result.rows;
+	if(rows.length) {
+		return rows[0];
+	}
+
+	return null;
 }
 
 export {
